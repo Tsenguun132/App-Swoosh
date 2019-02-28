0d864a1d2c27c83d8c898ec48a99c1d82f5d7818
Tsenguun Batbold <Tsenguun.batbold12@gmail.com>
2019-02-28 19:27:40 +0800

    Initial Commit

---
+++ app-swoosh/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  app-swoosh
+//
+//  Created by Tsenguun Batbold on 28/2/19.
+//  Copyright © 2019 Tsenguun Batbold. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
