bd6715af6b8cd216446d9519525d99a3668d10f3
Tsenguun Batbold <Tsenguun.batbold12@gmail.com>
2019-03-02 00:37:32 +0800

    new-update

---
+++ app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Tsenguun Batbold on 1/3/19.
+//  Copyright © 2019 Tsenguun Batbold. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String!
+    var selectedSKillLevel: String!
+}
